aa79ef02f743af53bc6b1e16bb1ad2c3766b0ebf
n.slszt ナスワサルサ <80197467+nslszt@users.noreply.github.com>
2021-04-05 01:23:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -35,3 +35,19 @@ class fb_infoga:
         print(f'\r{c}Please wait {a}{i} ',end="",flush=True)
         sleep(0.1)
     print(f"\nDONE !\n\n{result}")
+
+  def paginate(self,data,n):
+    n_data = round(len(data)/n) + 1
+    new_data_part = []
+    limit = 0
+    for i in range(n_data):
+      new_data = []
+      for x in range(limit,n+limit):
+        try:
+          new_data.append(data[x])
+        except:
+          pass
+        batas += 1
+      if new_data: new_data_part.append(new_data)
+    return new_data_part
+
