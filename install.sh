56ffbb521eb717f8f11a499a11f82b99d97d9d15
n.slszt ナスワサルサ <80197467+nslszt@users.noreply.github.com>
2021-04-04 23:55:16 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#########################
+#       FB-INFOGA       #
+#########################
+# Author : Cyberkallan  #
+# Version : 0.0.1       #
+#########################
+
+apt-get update && apt-get upgrade -y
+apt-get install python -y
+apt-get install sqlite -y
